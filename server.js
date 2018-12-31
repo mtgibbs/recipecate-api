f933a255b81752daad5dc737969639ba1985d2e0
Matt Gibbs <matt@mtgibbs.xyz>
2018-12-31 18:01:24 -0500

    stupid testing of knex query for list
    probably easier to just query twice

--- server.js
+++ server.js
@@ -7,23 +7,48 @@ const server = new Hapi.Server({
     host: 'localhost'
 });
 
-server.route({
-    path: '/recipes',
-    method: 'GET',
-    handler: async (request, h) => {
-        const results = await knex.from('recipe').select('id', 'name');
-        if (!results || results.length === 0) {
+
+// Recipe Routes
+server.route([
+    {
+        path: '/recipes',
+        method: 'GET',
+        handler: async (request, h) => {
+            const results = await knex.from('recipe').select('id', 'name');
+            if (!results || results.length === 0) {
+                return {
+                    count: 0,
+                    data: []
+                };
+            }
             return {
-                count: 0,
-                data: []
+                count: results.length,
+                data: results
             };
         }
-        return {
-            count: results.length,
-            data: results
-        };
+    },
+    {
+        path: '/recipes/{id}',
+        method: 'GET',
+        handler: async (request, h) => {
+
+            const recipeId = request.params.id;
+
+            const result = await
+                knex('recipe')
+                    .join('recipes_ingredients', { 'recipe.id': 'recipes_ingredients.recipe_id' })
+                    .join('ingredient', { 'ingredient.id': 'recipes_ingredients.ingredient_id' })
+                    .where({ 'recipe.id': recipeId })
+                    .select('*');
+
+            if (!result || result.length === 0) {
+                return h.response(null).code(404);
+            }
+
+            return result[0];
+        }
     }
-});
+]);
 
 const init = async () => {
     await server.start();
