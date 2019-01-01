158eddb36f29e821176dc4b0661ae36c8bf16d4d
Matt Gibbs <matt@mtgibbs.xyz>
2019-01-01 12:24:33 -0500

    Wiring up some swagger stuff

--- server.js
+++ server.js
@@ -1,18 +1,52 @@
 
 const Hapi = require('hapi');
 const knex = require('./knex');
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
 
 const server = new Hapi.Server({
     port: 8080,
     host: 'localhost'
 });
 
+const init = async () => {
 
-// Recipe Routes
+    const swaggerOptions = {
+        info: {
+            title: 'Recipecate API Documentation',
+            version: '0.0.1',
+        },
+    };
+
+    await server.register([
+        Inert,
+        Vision,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ]);
+
+    await server.start();
+    console.log(`Server started at ${server.info.uri}`);
+}
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
+
+// #region Routes
 server.route([
     {
         path: '/recipes',
         method: 'GET',
+        options: {
+            tags: ['api', 'recipes']
+        },
         handler: async (request, h) => {
             const results = await knex.from('recipe').select('id', 'name');
             if (!results || results.length === 0) {
@@ -30,6 +64,9 @@ server.route([
     {
         path: '/recipes/{id}',
         method: 'GET',
+        options: {
+            tags: ['api', 'recipes']
+        },
         handler: async (request, h) => {
 
             const recipeId = request.params.id;
@@ -55,14 +92,4 @@ server.route([
     }
 ]);
 
-const init = async () => {
-    await server.start();
-    console.log(`Server started at ${server.info.uri}`);
-}
-
-process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
-});
-
-init();
+// #endregion
