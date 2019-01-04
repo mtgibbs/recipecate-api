65441c78fffcf4e46e59b82910830d56d6d4904c
Matt Gibbs <matt@mtgibbs.xyz>
2019-01-03 19:51:01 -0500

    Add Meal Plan Schema

---
+++ migrations/20190103193217_Add-Meal-Plans.js
@@ -0,0 +1,36 @@
+
+exports.up = function (knex, Promise) {
+
+    return Promise.all(
+        [
+            knex.schema.createTable('meal_plan', table => {
+                table.increments('id').primary();
+                table.string('name', 200).notNullable();
+                table.datetime('created_date').notNullable();
+                table.string('notes', 500);
+            }),
+            knex.schema.createTable('meal_plan_recipe', table => {
+                table.integer('meal_plan_id').references('meal_plan.id');
+                table.integer('recipe_id').references('recipe.id');
+                table.unique(['meal_plan_id', 'recipe_id']);
+            }),
+            knex.schema.createTable('shopping_list', table => {
+                table.increments('id').primary();
+                table.integer('meal_plan_id').references('meal_plan.id');
+                table.integer('ingredient_id').references('ingredient.id');
+                table.float('amount').notNullable();
+                table.string('unit_of_measurement', 20).notNullable(); // TODO: Turn this into its own enum table
+
+                table.unique(['meal_plan_id', 'ingredient_id', 'unit_of_measurement']);
+            })
+        ]
+    )
+};
+
+exports.down = function (knex, Promise) {
+    return knex
+        .schema
+        .dropTableIfExists('shopping_list')
+        .dropTableIfExists('meal_plan_recipe')
+        .dropTableIfExists('meal_plan');
+};
