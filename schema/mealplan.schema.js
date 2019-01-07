b7222883c5a5c489ac719aadcdc01f68e88c17ad
Matt Gibbs <matt@mtgibbs.xyz>
2019-01-06 19:52:22 -0500

    Fix schema around meal plans

--- schema/mealplan.schema.js
+++ schema/mealplan.schema.js
@@ -4,7 +4,7 @@ const recipeSchemas = require('./recipe.schema');
 const mealPlanResponseSchema = Joi.object({
     id: Joi.number().integer().required(),
     name: Joi.string().required(),
-    notes: Joi.string().allow(null),
+    notes: Joi.string().allow(null, ''),
     createdDate: Joi.date().required()
 }).label('mealPlan');
 
@@ -18,10 +18,18 @@ const mealPlanDetailsResponseSchema = Joi.object({
     recipes: Joi.array().items(recipeSchemas.recipeListInfo).label('mealPlanRecipes')
 }).label('mealPlanDetails');
 
+const ingredientRequestSchema = Joi.object({
+    id: Joi.number().integer(),
+    name: Joi.string(),
+    amount: Joi.number().required().positive(),
+    unit_of_measurement: Joi.string().lowercase().required()
+}).label("shoppingListIngredient");
+
 const addMealPlanRequestSchema = Joi.object({
     name: Joi.string().required().max(200),
-    notes: Joi.string().max(500),
-    recipeIds: Joi.array().items(Joi.number().integer()).min(1)
+    notes: Joi.string().max(500).allow(null, ''),
+    recipeIds: Joi.array().items(Joi.number().integer()).min(1),
+    shoppingList: Joi.array().items(ingredientRequestSchema).allow(null).label('shoppingList')
 }).label('addMealPlanRequest');
 
 module.exports = {
