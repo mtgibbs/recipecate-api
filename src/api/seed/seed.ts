762a7ec4de578cad9fa950f3c1e71cc4bb1806dd
Matt Gibbs <matt@mtgibbs.xyz>
2024-03-25 21:17:48 -0400

    Update seed strategy
    This will try to add recipes that don't already exist in the database.  This is to make it easier for me to just keep tacking on until I have better support for modifying things

--- src/api/seed/seed.ts
+++ src/api/seed/seed.ts
@@ -4,14 +4,20 @@ import seedRecipes from './recipe.seed';
 const runSeed = async () => {
 
     try {
-        const count = await Recipe.countDocuments({});
+        console.log('Running recipe seed...');
 
-        if (count) {
-            return;
+        const existingRecipes = await Recipe.find({});
+        const existingRecipeNames = existingRecipes.map(recipe => recipe.name);
+
+        const newRecipes = seedRecipes.filter(recipe => !existingRecipeNames.includes(recipe.name));
+
+        if (newRecipes.length) {
+            await Recipe.insertMany(newRecipes);
+            console.log('New recipes added to the database:', newRecipes);
+        } else {
+            console.log('No new recipes found.');
         }
 
-        console.log('Empty Database Detected.  Seeding w/ initial recipes.');
-        await Recipe.insertMany(seedRecipes);
         console.log('Data seeded successfully.');
 
     } catch (err) {
